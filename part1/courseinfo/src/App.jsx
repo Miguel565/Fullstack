9b852f7fcc552171e0895edadd5650bb0f2ec6fd
Miguel Guerrero <miguel.guerrerog6@gmail.com>
2025-05-31 17:50:32 -0500

    1.2: Course information, step2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -7,12 +7,34 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  const Header = (props) => {
+    return (
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+    )
+  }
+  const Content = (props) => {
+    return (
+      <div>
+        <Part part={props.part1} exercises={props.exercises1} />
+        <Part part={props.part2} exercises={props.exercises2} />
+        <Part part={props.part3} exercises={props.exercises3} />
+      </div>
+    )
+  }
+  const Total = (props) => {
+     return (
+      <div>
+        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Header course ={course} />
-      <Content part1={part1} exercises1={exercises1} />
-      <Content part2={part2} exercises2={exercises2} />
-      <Content part3={part3} exercises3={exercises3} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
